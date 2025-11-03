40c16acf0c993e241a58afba86c5b736b9a46fa3
KevinCC09 <stephanokevhin0903@gmail.com>
2025-11-02 20:47:32 -0500

    orden

--- INVENTARIO_TIENDA/Program.cs
+++ INVENTARIO_TIENDA/Program.cs
@@ -10,40 +10,40 @@ class program
   static int totalProductos = 0;
 
   static void main(string[] args)
-  {
-    int opcion;
-    do
-{
-    MostrarMenu();
-    opcion = LeerOpcion();
+  { 
+        int opcion;
+        do
+        {
+            strarMenu();
+            opcion = LeerOpcion();
 
-    switch (opcion)
-    {
-        case 1:
-            AgregarProducto();
-            break;
-        case 2:
-            BuscarProducto();
-            break;
-        case 3:
-            MostrarInventario();
-            break;
-        case 0:
-            Console.WriteLine("Saliendo del sistema. ¡Hasta luego!");
-            break;
-        default:
-            Console.WriteLine("Opción no válida. Intente de nuevo.");
-            break;
-    }
+            switch (opcion)
+            {
+            case 1:
+                AgregarProducto();
+                break;
+            case 2:
+                BuscarProducto();
+                break;
+            case 3:
+                MostrarInventario();
+                break;
+                case 0:
+                Console.WriteLine("Saliendo del sistema. ¡Hasta luego!");
+                break;
+            default:
+                Console.WriteLine("Opción no válida. Intente de nuevo.");
+                break;
+            }
 
-    if (opcion != 0)
-    {
-        Console.WriteLine("\nPresione cualquier tecla para continuar...");
-        Console.ReadKey();
-        Console.Clear();
-    }
+            if (opcion != 0)
+            {
+                Console.WriteLine("\nPresione cualquier tecla para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+            }
 
-} while (opcion != 0);
+        }while (opcion != 0);
   }
 
 
