873171e63c00a3821af7824e148cfd92eda18432
Fernando-programacion <fb9286986@gmail.com>
2025-11-02 22:18:15 -0500

    Update Program.cs

--- INVENTARIO_TIENDA/Program.cs
+++ INVENTARIO_TIENDA/Program.cs
@@ -44,6 +44,54 @@ class program
             }
 
         }while (opcion != 0);
+            static void AgregarProducto()
+        {
+            if(totalProductos >= MAX_PRODUCTOS)
+            {
+                Console.WriteLine("EL inventario esta lleno. No se puede agregar mas productos.")return;
+
+            }
+
+            Console.WriteLine("Ingrese el nombre del producto: ")
+            string nombre= Console.ReadLine()?.Trim();
+
+            if(string.IsNu110rEmpty(nombre))
+            {
+                Console.WriteLine("El nombre no puede estar vacio. ")
+                return;
+            }
+            //Verificar si ya existe (opcional,según enunciado)
+            for(int i=0; i < totalProductos; i++)
+            {
+                if(nombre[i].Equals(nombre, stringComparison.Ordina1IgnoreCase) )
+                {
+                    Console.WriteLine($"El producto '{nombre}' ya existe en el inventario.");
+                    return;
+                }
+            }
+            Console.WriteLine("INgrese el precio del producto: $")
+            
+            if(!double.TryParce(Console.ReadLine(), out double precio )|| precio < 0)
+            {
+                Console.WriteLine("Precio Invalido");
+                return;
+            }
+
+            Console.WriteLine("Ingrese la cantidad en stock: ");
+            if(!int.TryParce(Console.ReadLine(), out int cantidad)||cantidad < 0)
+            {
+                Console.WriteLine("Cantidad inválida.")
+                return;
+            }
+
+            //Agregar inventario
+            nombres[totalProductos] = nombre;
+            precios[totalProductos] = precio;
+            stock[totalProductos] = cantidad;
+            totalProductos++;
+
+            Console.WriteLine($"Producto '{nombre} agregado exitosamente.")
+        }
   }
 
 
