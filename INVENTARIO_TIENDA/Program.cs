c2a5b8ff4216eae34524cf1e1ab5d9b5a9c601a3
KevinCC09 <stephanokevhin0903@gmail.com>
2025-11-02 20:18:01 -0500

    cambios1

--- INVENTARIO_TIENDA/Program.cs
+++ INVENTARIO_TIENDA/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
-namespace INVENTARIO_TIENDA
+namespace INVENTARIO_TIENDA;
 class program
 {
   const int MAX_PRODUCTOS = 5;
   static string[] nombres = new string [MAX_PRODUCTOS]; 
+  
 }
 
 
