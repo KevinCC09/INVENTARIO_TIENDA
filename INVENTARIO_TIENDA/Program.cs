2bcca6d44d6db2ef8a5bf33441b6e1b4e5af903a
KevinCC09 <stephanokevhin0903@gmail.com>
2025-11-02 20:19:41 -0500

    arrays

--- INVENTARIO_TIENDA/Program.cs
+++ INVENTARIO_TIENDA/Program.cs
@@ -4,8 +4,12 @@ namespace INVENTARIO_TIENDA;
 class program
 {
   const int MAX_PRODUCTOS = 5;
-  static string[] nombres = new string [MAX_PRODUCTOS]; 
-  
+  static string[] nombres = new string [MAX_PRODUCTOS];
+  static double[] precios = new double[MAX_PRODUCTOS];
+  static int[] stock = new int[MAX_PRODUCTOS];
+  static int totalProductos = 0;
+
+
 }
 
 
